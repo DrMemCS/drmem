2bfbda2f06e744fac7fb07135a4daa90c6762371
Rich Neswold <rich.neswold@gmail.com>
2021-03-12 00:47:10 -0600

    feat: add tests for the encoders and decoders.

--- src/device/data.rs
+++ src/device/data.rs
@@ -196,17 +196,116 @@ impl Compat for String {
 
 #[cfg(test)]
 mod tests {
-    use redis::from_redis_value;
+    use redis::{ from_redis_value, Value };
     use super::*;
 
+    // We only want to convert Value::Data() forms. These tests make
+    // sure the other variants don't translate.
+
     #[tokio::test]
-    async fn test_decoders() {
-	assert_ne!(Ok(Type::Nil), from_redis_value(&redis::Value::Nil));
+    async fn test_reject_invalid_forms() {
+	if let Ok(v) = from_redis_value::<Type>(&Value::Nil) {
+	    panic!("Value::Nil incorrectly translated to {:?}", v);
+	}
+	if let Ok(v) = from_redis_value::<Type>(&Value::Int(0)) {
+	    panic!("Value::Int incorrectly translated to {:?}", v);
+	}
+	if let Ok(v) = from_redis_value::<Type>(&Value::Bulk(vec![])) {
+	    panic!("Value::Bulk incorrectly translated to {:?}", v);
+	}
+	if let Ok(v) = from_redis_value::<Type>(&Value::Status(String::from(""))) {
+	    panic!("Value::Status incorrectly translated to {:?}", v);
+	}
+	if let Ok(v) = from_redis_value::<Type>(&Value::Okay) {
+	    panic!("Value::Okay incorrectly translated to {:?}", v);
+	}
+    }
+
+    // Test correct Type::Nil decoding.
+
+    #[tokio::test]
+    async fn test_nil_decoder() {
+	assert_eq!(Ok(Type::Nil),
+		   from_redis_value::<Type>(&Value::Data(vec![])));
+    }
+
+    // Test correct Type::Bool decoding.
+
+    #[tokio::test]
+    async fn test_bool_decoder() {
+	assert_eq!(Ok(Type::Bool(false)),
+		   from_redis_value::<Type>(&Value::Data(vec!['F' as u8])));
+	assert_eq!(Ok(Type::Bool(true)),
+		   from_redis_value::<Type>(&Value::Data(vec!['T' as u8])));
+    }
+
+    // Test correct Type::Int decoding.
+
+    #[tokio::test]
+    async fn test_int_decoder() {
+	let values: Vec<(i64, Vec<u8>)> = vec![
+	    (0, vec!['I' as u8,
+		     0x00u8, 0x00u8, 0x00u8, 0x00u8,
+		     0x00u8, 0x00u8, 0x00u8, 0x00u8]),
+	    (1, vec!['I' as u8,
+		     0x00u8, 0x00u8, 0x00u8, 0x00u8,
+		     0x00u8, 0x00u8, 0x00u8, 0x01u8]),
+	    (-1, vec!['I' as u8,
+		      0xffu8, 0xffu8, 0xffu8, 0xffu8,
+		      0xffu8, 0xffu8, 0xffu8, 0xffu8]),
+	    (0x7fffffffffffffff, vec!['I' as u8,
+				      0x7fu8, 0xffu8, 0xffu8, 0xffu8,
+				      0xffu8, 0xffu8, 0xffu8, 0xffu8]),
+	    (-0x8000000000000000, vec!['I' as u8,
+				       0x80u8, 0x00u8, 0x00u8, 0x00u8,
+				       0x00u8, 0x00u8, 0x00u8, 0x00u8]),
+	    (0x0123456789abcdef, vec!['I' as u8,
+				      0x01u8, 0x23u8, 0x45u8, 0x67u8,
+				      0x89u8, 0xabu8, 0xcdu8, 0xefu8]),
+	];
+
+	for (v, rv) in values.iter() {
+	    let data = Value::Data(rv.to_vec());
+
+	    assert_eq!(Ok(Type::Int(*v)), from_redis_value(&data));
+	}
     }
 
     #[tokio::test]
-    async fn test_encoders() {
-	assert!(true);
+    async fn test_nil_encoder() {
+	assert_eq!(vec![ Vec::<u8>::new() ], Type::Nil.to_redis_args());
     }
 
+    #[tokio::test]
+    async fn test_bool_encoder() {
+	assert_eq!(vec![ vec![ 'F' as u8] ],
+		   Type::Bool(false).to_redis_args());
+	assert_eq!(vec![ vec![ 'T' as u8] ],
+		   Type::Bool(true).to_redis_args());
+    }
+
+    #[tokio::test]
+    async fn test_int_encoder() {
+	let values: Vec<(i64, Vec<Vec<u8>>)> = vec![
+	    (0, vec![ vec![ 'I' as u8, 0x00, 0x00, 0x00, 0x00,
+			     0x00, 0x00, 0x00, 0x00 ] ]),
+	    (1, vec![ vec![ 'I' as u8, 0x00, 0x00, 0x00, 0x00,
+			     0x00, 0x00, 0x00, 0x01 ] ]),
+	    (-1, vec![ vec![ 'I' as u8, 0xff, 0xff, 0xff, 0xff,
+			      0xff, 0xff, 0xff, 0xff ] ]),
+	    (0x7fffffffffffffff,
+	     vec![ vec![ 'I' as u8, 0x7f, 0xff, 0xff, 0xff,
+			  0xff, 0xff, 0xff, 0xff ] ]),
+	    (-0x8000000000000000,
+	     vec![ vec![ 'I' as u8, 0x80, 0x00, 0x00, 0x00,
+			  0x00, 0x00, 0x00, 0x00 ] ]),
+	    (0x0123456789abcdef,
+	     vec![ vec![ 'I' as u8, 0x01, 0x23, 0x45, 0x67,
+			  0x89, 0xab, 0xcd, 0xef ] ]),
+	];
+
+	for (v, rv) in values.iter() {
+	    assert_eq!(*rv, Type::Int(*v).to_redis_args());
+	}
+    }
 }
