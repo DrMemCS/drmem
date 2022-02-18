3d10bfe07cf4a3601dd314e6386991231def6d7f
Rich Neswold <rich.neswold@gmail.com>
2022-02-17 23:08:11 -0600

    :recycle: tweak driver API

--- drmem-api/src/driver.rs
+++ drmem-api/src/driver.rs
@@ -28,6 +28,9 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+//! This module defines types and interfaces that driver use to
+//! interact with the core of DrMem.
+
 use async_trait::async_trait;
 use tokio::sync::{broadcast, mpsc, oneshot};
 use toml::value;
@@ -35,10 +38,173 @@ use drmem_types::{DeviceValue, DrMemError};
 
 use super::Result;
 
-/// All drivers implement the `Driver` trait.
+pub type Config = value::Table;
+pub type TxDeviceValue = broadcast::Sender<DeviceValue>;
+pub type TxDeviceSetting = mpsc::Sender<DeviceValue>;
+pub type RxDeviceSetting = mpsc::Receiver<DeviceValue>;
+
+/// Defines the requests that can be sent to core.
+#[derive(Debug)]
+pub enum Request {
+    /// Registers a read-only device with core.
+    ///
+    /// The reply will contain a channel to broadcast values read from
+    /// the hardware.
+    AddReadonlyDevice {
+        dev_name: String,
+        rpy_chan: oneshot::Sender<Result<TxDeviceValue>>,
+    },
+
+    /// Registers a writable device with core.
+    ///
+    /// The reply is a pair where the first element is a channel to
+    /// broadcast values read from the hardware. The second element is
+    /// a read-handle to acccept incoming setting to the device.
+    AddReadWriteDevice {
+        dev_name: String,
+        rpy_chan: oneshot::Sender<Result<(TxDeviceValue, RxDeviceSetting)>>,
+    },
+}
+
+pub type Reading<T> = Box<
+    dyn Fn(
+        T,
+    ) -> std::result::Result<
+        usize,
+        broadcast::error::SendError<DeviceValue>,
+    >,
+>;
+
+/// A handle which is used to communicate with the core of DrMem.
+/// When a driver is created, it will be given a handle to be used
+/// throughout its life.
+///
+/// This type wraps the `mpsc::Sender<>` and defines a set of helper
+/// methods to send requests and receive replies with the core.
+pub struct RequestChan {
+    prefix: String,
+    req_chan: mpsc::Sender<Request>,
+}
+
+impl RequestChan {
+    pub fn new(prefix: &str, req_chan: &mpsc::Sender<Request>) -> Self {
+        RequestChan {
+            prefix: String::from(prefix),
+            req_chan: req_chan.clone(),
+        }
+    }
+
+    /// Registers a read-only device with the framework. `name` is the
+    /// last section of the full device name. Typically a driver will
+    /// register several devices, each representing a portion of the
+    /// hardware being controlled. All devices for a given driver
+    /// instance will have the same prefix; the `name` parameter is
+    /// appended to it.
+    ///
+    /// If it returns `Ok()`, the value is a broadcast channel that
+    /// the driver uses to announce new values of the associated
+    /// hardware.
+    ///
+    /// If it returns `Err()`, the underlying value could be `InUse`,
+    /// meaning the device name is already registered. If the error is
+    /// `InternalError`, then the core has exited and the
+    /// `RequestChan` has been closed. Since the driver can't report
+    /// any more updates, it may as well shutdown.
+    pub async fn add_ro_device<T: Into<DeviceValue>>(
+        &self, name: &str,
+    ) -> super::Result<Reading<T>> {
+        // Create a location for the reply.
+
+        let (tx, rx) = oneshot::channel();
+
+        // Send a request to Core to register the given name.
+        //
+        // XXX: Device names should be handled more formally. This
+        // code doesn't check that the names are of the correct
+        // character set.
+
+        let result = self
+            .req_chan
+            .send(Request::AddReadonlyDevice {
+                dev_name: format!("{}:{}", self.prefix, name),
+                rpy_chan: tx,
+            })
+            .await;
+
+        // If the request was sent successfully and we successfully
+        // received a reply, process the payload.
+
+        if result.is_ok() {
+            match rx.await {
+                // If the name was successfully registered, core will
+                // return a sending handle for device readings. Wrap
+                // the handle in a type-safe wrapper (so the driver
+                // can only send the correct type.)
+                Ok(Ok(ch)) => {
+                    return Ok(Box::new(move |v: T| ch.send(v.into())))
+                }
+
+                Ok(Err(e)) => return Err(e),
+
+                Err(_) => (),
+            }
+        }
+
+        // If either communication direction failed, return an error
+        // indicating we can't talk to core.
+
+        Err(DrMemError::MissingPeer(String::from("core")))
+    }
+
+    /// Registers a read-write device with the framework. `name` is the
+    /// last section of the full device name. Typically a driver will
+    /// register several devices, each representing a portion of the
+    /// hardware being controlled. All devices for a given driver
+    /// instance will have the same prefix; the `name` parameter is
+    /// appended to it.
+    ///
+    /// If it returns `Ok()`, the value is a pair containing a
+    /// broadcast channel that the driver uses to announce new values
+    /// of the associated hardware and a receive channel for incoming
+    /// settings to be applied to the hardware.
+    ///
+    /// If it returns `Err()`, the underlying value could be `InUse`,
+    /// meaning the device name is already registered. If the error is
+    /// `InternalError`, then the core has exited and the
+    /// `RequestChan` has been closed. Since the driver can't report
+    /// any more updates or accept new settings, it may as well shutdown.
+    pub async fn add_rw_device(
+        &self, name: &str,
+    ) -> Result<(
+        broadcast::Sender<DeviceValue>,
+        mpsc::Receiver<DeviceValue>,
+    )> {
+        let (tx, rx) = oneshot::channel();
+	let result = self
+            .req_chan
+            .send(Request::AddReadWriteDevice {
+                dev_name: format!("{}:{}", self.prefix, name),
+                rpy_chan: tx,
+            })
+            .await;
+
+        if result.is_ok() {
+            if let Ok(v) = rx.await {
+                return v;
+            }
+        }
+        Err(DrMemError::MissingPeer(String::from("core")))
+    }
+}
+
+/// All drivers implement the `driver::API` trait.
 #[async_trait]
-pub trait Driver {
-    async fn run(&mut self) -> Result<()>;
+pub trait API {
+    fn create(cfg: Config, drc: RequestChan) -> Result<Box<dyn API>>
+    where
+        Self: Sized;
+
+    async fn run(mut self) -> Result<()>;
 
     /// The name of the driver. This should be relatively short, but
     /// needs to be unique across all drivers.
