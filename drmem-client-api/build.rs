97224bb63244363ee63e6c9c8f16621c589df53b
Rich Neswold <rich.neswold@gmail.com>
2022-02-20 16:25:43 -0600

    :sparkles: create `drmem-client-api` crate
    This is the start of a crate that client applications can use to
    communicate with DrMem. It contains an initial gRPC protocol file.

---
+++ drmem-client-api/build.rs
@@ -0,0 +1,21 @@
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let iface_files = &[
+        "proto/drmem.proto",
+    ];
+    let dirs = &["proto"];
+
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(iface_files, dirs)?;
+
+    // recompile protobufs only if any of the proto files changes.
+    for file in iface_files {
+        println!("cargo:rerun-if-changed={}", file);
+    }
+
+    Ok(())
+}
