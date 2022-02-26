22e086e778e1f453d2e8f36474f632b8d65f413e
Rich Neswold <rich.neswold@gmail.com>
2022-02-26 12:09:22 -0600

    :recycle: only build server bindings to gRPC API

--- drmem-client-api/build.rs
+++ drmem-client-api/build.rs
@@ -1,19 +1,22 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let iface_files = &[
-        "proto/drmem.proto",
-    ];
+    let idl_src = &["proto/drmem.proto"];
     let dirs = &["proto"];
 
+    // Only build the server bindings. The DrMem project doesn't
+    // include any client applications. If one wants to write a
+    // client, they can take the `.proto` file and generate the
+    // appropriate bindings.
+
     tonic_build::configure()
-        .build_client(true)
         .build_server(true)
         .protoc_arg("--experimental_allow_proto3_optional")
-        .compile(iface_files, dirs)?;
+        .compile(idl_src, dirs)?;
 
     // recompile protobufs only if any of the proto files changes.
-    for file in iface_files {
+
+    for file in idl_src {
         println!("cargo:rerun-if-changed={}", file);
     }
 
