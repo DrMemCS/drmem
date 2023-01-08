c71167dbe16e25706de0c170f461ee843bb1d793
Rich Neswold <rich.neswold@gmail.com>
2023-01-08 01:15:53 -0600

    :sparkles: stop error recovery in parsing
    The lex/yacc crate we're using has a powerful recovery system when
    they can't parse a grammar. In our case, our expressions are one line
    long, instead of a file full of source code. And, being an embedded
    system, we can't easily provide the user with all the corrective
    recommendations. So this commit disables the error correction and
    simply tells the user where in the line there was an error.

--- drmemd/build.rs
+++ drmemd/build.rs
@@ -5,6 +5,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     CTLexerBuilder::new()
         .lrpar_config(|ctp| {
             ctp.yacckind(YaccKind::Grmtools)
+		.recoverer(lrpar::RecoveryKind::None)
                 .grammar_in_src_dir("logic/logic.y")
                 .unwrap()
         })
